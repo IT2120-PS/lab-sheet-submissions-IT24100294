234938fed8149362e7dc9035ebe4fdff9645d041
IT24100294 <it24100294@my.sliit.lk>
2025-09-02 21:52:08 +0530

    Lab 6

---
+++ Lab Sheet 06/PS_Lab_6.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\Virul Akbo\\OneDrive - Sri Lanka Institute of Information Technology\\Y2S1\\PS\\Labs\\Lab6")
+
+#Q1)
+#i.Binomial distribution with =50 and p=0.85
+#ii.
+1-pbinom(47,50,0.85,lower.tail = TRUE)
+
+
+#Q2)
+#i.Number of students students is selected at random who have completed the course using this platform.
+
+#ii.Possion
+#iii.
+dpois(15,12)
